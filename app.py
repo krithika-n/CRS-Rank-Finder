498136192156cf60f78f19d087a6f6460e5e9eac
Krithika Narayanan <krithikanarayanan@asu.edu>
2023-10-03 23:10:46 -0700

    adding commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, g, jsonify
 import requests
 import json
 from score import ScoreMap
@@ -7,22 +7,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return jsonify({"Success": "OK"}), 200
 
-    filename = 'data.json'
-
-    #url = 'https://www.canada.ca/content/dam/ircc/documents/json/ee_rounds_123_en.json'  # Replace with the actual URL
+def get_data():
+    url = 'https://www.canada.ca/content/dam/ircc/documents/json/ee_rounds_123_en.json'
     try:
-        #response = requests.get(url)
-        #return render_template('index.html', data=data)
-        with open(filename, 'r') as file:
-            data = json.load(file)
-        currentScore = ScoreMap(data['rounds'][0])
-        return ScoreMap.getRange(423)
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json().get('rounds', [])
+        g.data = data
+        return data
+    except requests.RequestException as e:
+        app.logger.error(f"Error fetching data from {url}: {e}")
+        return None
 
-    except Exception as e:
-        print(f"Error fetching data: {e}")
-        return "Internal Server Error", 500
-
+@app.route('/rank/<score>', methods=['GET'])
+def get_approx_rank(score):
+    try:
+        score = int(score)
+        if not(0 <= score and score <= 1200):
+            raise ValueError("Score must be within 0 - 1200")
+    except ValueError as ve:  # Catch exceptions raised by invalid input or manual raise
+        app.logger.error(f"Error: {str(ve)}.")
+        return jsonify({"error": str(ve)}), 400  # Returns a 400 Bad Request response with error message
+    data = getattr(g, 'data', None)
+    if data is None:
+        get_data()
+        data = g.data
+    if len(data) > 1:
+        latest_score_json = data[0]
+    current_score = ScoreMap(latest_score_json)
+    return str(current_score.calculate_approx_rank(int(score)))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- score.py
+++ score.py
@@ -1,74 +1,74 @@
 from bisect import bisect
 import json
 
-class ScoreMap:
-
-    def __init__(self):
-        self.rangeMap = {}
-        self.rangeMap[Range(601, 1200)] = 0
-        self.rangeMap[Range(501-600)] = 0
-        self.rangeMap[Range(491,500)] = 0
-        self.rangeMap[Range(481, 490)] = 0
-        self.rangeMap[Range(471, 480)] = 0
-        self.rangeMap[Range(461,470)] = 0
-        self.rangeMap[Range(451, 460)] = 0
-        self.rangeMap[Range(441, 450)] = 0
-        self.rangeMap[Range(431, 440)] = 0
-        self.rangeMap[Range(421, 430)] = 0
-        self.rangeMap[Range(411, 420)] = 0
-        self.rangeMap[Range(401, 410)] = 0
-        self.rangeMap[Range(351, 400)] = 0
-        self.rangeMap[Range(301, 350)] = 0
-        self.rangeMap[Range(0, 300)] = 0
-        self.aggregatedRangeMap = {}
-        self.aggregatedRangeMap[Range(601, 1200)] = 0
-        self.aggregatedRangeMap[Range(501, 600)] = 0
-        self.aggregatedRangeMap[Range(451, 500)] = 0
-        self.aggregatedRangeMap[Range(401, 450)] = 0
-        self.aggregatedRangeMap[Range(351, 400)] = 0
-        self.aggregatedRangeMap[Range(301, 350)] = 0
-        self.aggregatedRangeMap[Range(0, 300)] = 0
-        self.total = 0
-
-    def __init__(self, data: json):
-        self.rangeMap = {}
-        self.rangeMap[Range(601, 1200)] = data['dd1']
-        self.rangeMap[Range(501-600)] = data['dd2']
-        self.rangeMap[Range(491,500)] = data['dd4']
-        self.rangeMap[Range(481, 490)] = data['dd5']
-        self.rangeMap[Range(471, 480)] = data['dd6']
-        self.rangeMap[Range(461,470)] = data['dd7']
-        self.rangeMap[Range(451, 460)] = data['dd8']
-        self.rangeMap[Range(441, 450)] = data['dd10']
-        self.rangeMap[Range(431, 440)] = data['dd11']
-        self.rangeMap[Range(421, 430)] = data['dd12']
-        self.rangeMap[Range(411, 420)] = data['dd13']
-        self.rangeMap[Range(401, 410)] = data['dd14']
-        self.rangeMap[Range(351, 400)] = data['dd15']
-        self.rangeMap[Range(301, 350)] = data['dd16']
-        self.rangeMap[Range(0, 300)] = data['dd17']
-        self.aggregatedRangeMap = {}
-        self.aggregatedRangeMap[Range(601, 1200)] = data['dd1']
-        self.aggregatedRangeMap[Range(501, 600)] = data['dd2']
-        self.aggregatedRangeMap[Range(451, 500)] = data['dd3']
-        self.aggregatedRangeMap[Range(401, 450)] = data['dd9']
-        self.aggregatedRangeMap[Range(351, 400)] = data['dd15']
-        self.aggregatedRangeMap[Range(301, 350)] = data['dd16']
-        self.aggregatedRangeMap[Range(0, 300)] = data['dd17']
-        self.total = data['dd18']
-
-
-    def getRange(self, score):
-        bottomRanges = [601, 501, 491, 481, 471, 461, 451, 441, 431, 421, 411, 401, 351, 301, 0]
-        insertPoint = bisect(bottomRanges, score)
-        return insertPoint
-
 class Range:
     
-    def __init__(self, top, bottom):
+    def __init__(self, bottom: int, top: int):
         self.top = top
         self.bottom = bottom
 
-    def isInRange(self, score) -> bool:
-        return self.top <= score <= self.bottom
+    def is_in_range(self, score: int) -> bool:
+        return self.bottom <= score <= self.top
+
+    def __str__(self):
+        return "(" + self.top.__str__() + " - " + self.bottom.__str__() + ")"
+
+class ScoreMap:
+
+    max_range_value = 1200
+
+    def __init__(self, data: json):
+        self.range_map = {}
+        self.aggregated_range_map = {}
+        self.total = data['dd18']
+        self.range_map[Range(601, 1200)] = data['dd1']
+        self.range_map[Range(501, 600)] = data['dd2']
+        self.range_map[Range(491, 500)] = data['dd4']
+        self.range_map[Range(481, 490)] = data['dd5']
+        self.range_map[Range(471, 480)] = data['dd6']
+        self.range_map[Range(461, 470)] = data['dd7']
+        self.range_map[Range(451, 460)] = data['dd8']
+        self.range_map[Range(441, 450)] = data['dd10']
+        self.range_map[Range(431, 440)] = data['dd11']
+        self.range_map[Range(421, 430)] = data['dd12']
+        self.range_map[Range(411, 420)] = data['dd13']
+        self.range_map[Range(401, 410)] = data['dd14']
+        self.range_map[Range(351, 400)] = data['dd15']
+        self.range_map[Range(301, 350)] = data['dd16']
+        self.range_map[Range(0, 300)] = data['dd17']
+        self.aggregated_range_map[Range(601, 1200)] = data['dd1']
+        self.aggregated_range_map[Range(501, 600)] = data['dd2']
+        self.aggregated_range_map[Range(451, 500)] = data['dd3']
+        self.aggregated_range_map[Range(401, 450)] = data['dd9']
+        self.aggregated_range_map[Range(351, 400)] = data['dd15']
+        self.aggregated_range_map[Range(301, 350)] = data['dd16']
+        self.aggregated_range_map[Range(0, 300)] = data['dd17']
+        self.range_lookup = [None] * (self.max_range_value + 1)
+        for interval in self.range_map.keys():
+            for x in range(interval.bottom, interval.top + 1):
+                self.range_lookup[x] = interval
+
+    def get_range(self, score: int) -> Range:
+        return self.range_lookup[score]
     
+    def calculate_approx_rank(self, score: int) -> int:
+        if 0 <= score and score <= 1200:
+            rank = 0
+            for interval, number in self.range_map.items():
+                number = number.replace(",", "")
+                number = int(number)
+                if interval.bottom <= score  and score <= interval.top:
+                    # score lies within this range
+                    total_range = interval.top - interval.bottom + 1
+                    remaining_range = interval.top - score
+                    proportion = remaining_range / total_range
+                    rank += int(number * proportion)
+                elif interval.bottom > score:
+                    # score is below this range
+                    rank += number
+                else:
+                    # score is above this range
+                    continue
+            return 1 if rank == 0 else rank
+        else:
+            return -1
